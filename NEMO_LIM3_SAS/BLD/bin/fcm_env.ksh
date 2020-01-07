80df148465df6cf0c1c44792abe0a0852c121708
Guillaume BOUTIN <gboutin@ifremer.fr>
2020-01-07 09:31:08 +0000

    Add License, config, remove BLD

--- NEMO_LIM3_SAS/BLD/bin/fcm_env.ksh
+++
@@ -1,1 +0,0 @@
-/home1/datawork/gboutin/trunk/NEMOGCM/CONFIG/TEST_CASES/clean_with_clement/BLD/fcm_env.sh
--- NEMO_LIM3_SAS/BLD/fcm_env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-PATH=/home1/datawork/gboutin/trunk/NEMOGCM/CONFIG/TEST_CASES/clean_with_clement/BLD/bin:$PATH
-export PATH
